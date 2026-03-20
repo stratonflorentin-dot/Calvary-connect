e1778c1bfc685592ed1d84ccb78edfcecfd95829
Straton Tesha <stratonflorentin@gmail.com>
2026-03-20 09:04:36 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-8550736767-b9073",
+  "appId": "1:199053972563:web:274f93a9bb770edb5722a6",
+  "apiKey": "AIzaSyDMzWyeYQtc_4fgiSzNMw4eR1Npa8wPPWc",
+  "authDomain": "studio-8550736767-b9073.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "199053972563"
+};
