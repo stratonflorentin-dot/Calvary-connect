fc0aa8bd1ba84d243ada689dbe86c0bc6565e3da
elonixtz-dotcom <your-email@elonix.com>
2026-05-20 11:22:20 +0300

    Auto-fix user status to active upon successful login and during signup link

--- force_push.js
+++ force_push.js
@@ -1,13 +1,20 @@
 const { execSync } = require('child_process');
-const fs = require('fs');
 
 try {
+  console.log('--- GIT STATUS ---');
+  console.log(execSync('git status').toString());
+  
+  console.log('--- GIT ADD ---');
   console.log(execSync('git add .').toString());
+  
+  console.log('--- GIT COMMIT ---');
   try {
     console.log(execSync('git commit -m "Auto-fix user status to active upon successful login and during signup link"').toString());
   } catch(e) {
     console.log('Nothing to commit');
   }
+  
+  console.log('--- GIT PUSH ---');
   console.log(execSync('git push').toString());
 } catch (e) {
   console.error(e.stdout ? e.stdout.toString() : e.message);
